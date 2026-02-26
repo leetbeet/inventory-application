5153bb781f56eed95cee473a9f646405491cb2fb
leetbeet <136890775+leetbeet@users.noreply.github.com>
2026-02-26 09:19:40 +0800

    feat: add CRUD functions for item

---
+++ models/itemModel.js
@@ -0,0 +1,36 @@
+const pool = require("../db/pool");
+
+async function getAllItems() {
+  const result = await pool.query("SELECT * FROM item ORDER BY id");
+  return result.rows;
+}
+
+async function addItem(name, categoryId, brand, quantity) {
+  const result = await pool.query(
+    "INSERT INTO item (name, categoryId, brand, quantity) VALUES ($1, $2, $3, $4) RETURNING *",
+    [name, categoryId, brand, quantity],
+  );
+  return result.rows[0];
+}
+
+async function updateItem(name, categoryId, brand, quantity, id) {
+  const result = await pool.query(
+    "UPDATE item SET name=$1, categoryId=$2, brand=$3, quantity=$4 WHERE id=$5 RETURNING *",
+    [name, categoryId, brand, quantity, id],
+  );
+  return result.rows[0];
+}
+
+async function deleteItem(id) {
+  const result = await pool.query("DELETE FROM item WHERE id=$1 RETURNING *", [
+    id,
+  ]);
+  return result.rows[0];
+}
+
+module.exports = {
+  getAllItems,
+  addItem,
+  updateItem,
+  deleteItem,
+};
