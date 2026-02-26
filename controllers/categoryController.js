5af740f8fd702640e2b6d9ac60117205c5795622
leetbeet <136890775+leetbeet@users.noreply.github.com>
2026-02-26 11:50:05 +0800

    feat: add functions for categoryController

---
+++ controllers/categoryController.js
@@ -0,0 +1,53 @@
+const categoryModel = require("../models/categoryModel");
+const itemModel = require("../models/itemModel");
+
+async function list(req, res) {
+  const categories = await categoryModel.getAll();
+  res.render("category/list", { categories });
+}
+
+async function listItems(req, res) {
+  const category = await categoryModel.getById(req.params.id);
+  const items = await itemModel.getByCategory(req.params.id);
+  res.render("item/list", { category, items });
+}
+
+function showCreate(req, res) {
+  res.render("category/form");
+}
+
+async function create(req, res) {
+  await categoryModel.create(req.body.name);
+  res.redirect("/categories");
+}
+
+async function showUpdate(req, res) {
+  const category = await categoryModel.getById(req.params.id);
+  res.render("category/form", { category });
+}
+
+async function update(req, res) {
+  await categoryModel.update(req.body.name, req.params.id);
+  res.redirect("/categories");
+}
+
+async function showRemove(req, res) {
+  const category = await categoryModel.getById(req.params.id);
+  res.render("category/delete", { category });
+}
+
+async function remove(req, res) {
+  await categoryModel.remove(req.params.id);
+  res.redirect("/categories");
+}
+
+module.exports = {
+  list,
+  listItems,
+  showCreate,
+  create,
+  showUpdate,
+  update,
+  showRemove,
+  remove,
+};
