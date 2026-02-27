225709b50657b1940de8eaa52b71f949c300f920
leetbeet <136890775+leetbeet@users.noreply.github.com>
2026-02-27 11:56:45 +0800

    feat: add main routes to app.js

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
+const itemRoutes = require("../routes/itemRoutes");
+const categoryRoutes = require("../routes/categoryRoutes");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.use("/items", itemRoutes);
+app.use("/categories", categoryRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (error) => {
