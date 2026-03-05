0c6777cf162ad7ffa5cced2b8d4eb9d4f56bf1f4
leetbeet <136890775+leetbeet@users.noreply.github.com>
2026-03-05 12:47:29 +0800

    refactor: length check

--- middleware/validateCategory.js
+++ middleware/validateCategory.js
@@ -1,5 +1,11 @@
 const { body } = require("express-validator");
 
 exports.validateCategory = [
-  body("name").trim().escape().notEmpty().withMessage("Name required"),
+  body("name")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("Name required")
+    .isLength({ max: 15 })
+    .withMessage("Name must be at most 15 characters long"),
 ];
--- middleware/validateItem.js
+++ middleware/validateItem.js
@@ -1,28 +1,38 @@
 const { body } = require("express-validator");
 
 exports.validateItem = [
-  body("name").trim().escape().notEmpty().withMessage("Name required"),
+  body("name")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("Name required")
+    .isLength({ max: 15 })
+    .withMessage("Name must be at most 15 characters long"),
   body("categoryId")
     .trim()
     .escape()
     .notEmpty()
     .withMessage("Category ID required")
-    .isInt({ min: 1 })
-    .withMessage("Category ID must be an integer greater than or equal to 1")
+    .isInt({ min: 1, max: 10000 })
+    .withMessage(
+      "Category ID must be an integer greater than or equal to 1 and less than or equal to 100000",
+    )
     .toInt(),
   body("brand")
     .trim()
     .escape()
     .notEmpty()
     .withMessage("Brand required")
-    .isLength({ min: 1, max: 200 })
-    .withMessage("Brand must be between 1 and 200 characters"),
+    .isLength({ min: 1, max: 15 })
+    .withMessage("Brand must be between 1 and 15 characters"),
   body("quantity")
     .trim()
     .escape()
     .notEmpty()
     .withMessage("Quantity required")
-    .isInt({ min: 1 })
-    .withMessage("Quantity must be an integer greater than or equal to 1")
+    .isInt({ min: 1, max: 10000 })
+    .withMessage(
+      "Quantity must be an integer greater than or equal to 1 and less than or equal to 10000",
+    )
     .toInt(),
 ];
