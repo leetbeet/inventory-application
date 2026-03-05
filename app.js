d3e95449d0723fb95bd36afa9adb01a04c0a7def
leetbeet <136890775+leetbeet@users.noreply.github.com>
2026-03-05 11:08:45 +0800

    feat: add layout styling

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const categoryRoutes = require("./routes/categoryRoutes");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
 
 app.get("/", (req, res) => {
   res.render("index");
